31e82ac58a7fb18ae1e8fd5673c714dee5b8fa27
kaidornel <kai_dornel@hotmail.com>
2020-10-05 18:16:23 -0400

    finished part 1

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -2,23 +2,38 @@
 
 import React from 'react'
 
-const FormComponent = (props) => {
-    return (
-        <div className="form-component">
-            <br/><br/><br/><br/><br/>
-            <form className="form">
-                    <label>Username: </label>
-                    <input id='email' onChange={null} name='email' type='text' />
-                    <label>Password: </label>
-                    <input id='password' onChange={null} name='password' type='password' />
-                    <br/><br/>
-                    <label>Input field: </label>
-                    <input id='another input field' onChange={null} name='another input field' type='text' />
-                    <label>Input field: </label>
-                    <input id='last input field' onChange={null} name='last input field' type='text' />
-            </form>
-        </div>
-    )
+class FormComponent extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            anotherInputField: '',
+            lastInputField: ''
+        }
+    }
+    handleInputChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    render(){
+        return (
+            <div className="form-component">
+                <br/><br/><br/><br/><br/>
+                <form className="form">
+                        <label>Username: </label>
+                        <input id='email' onChange={this.handleInputChange} name='email' type='text' />
+                        <label>Password: </label>
+                        <input id='password' onChange={this.handleInputChange} name='password' type='password' />
+                        <br/><br/>
+                        <label>Input field: </label>
+                        <input id='another-input-field' onChange={this.handleInputChange} name='anotherInputField' type='text' />
+                        <label>Input field: </label>
+                        <input id='last-input-field' onChange={this.handleInputChange} name='lastInputField' type='text' />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default FormComponent
--- src/tests/FormComponent.test.js
+++ src/tests/FormComponent.test.js
@@ -8,6 +8,65 @@ describe('Form Component', () => {
     it('should render without throwing an error', () => {
       expect(shallow(<FormComponent />).find('div.form-component').exists()).toBe(true)
     })
+
+    it('renders a email input', () => {
+        expect(shallow(<FormComponent />).find('#email').length).toEqual(1)
+    })
+
+    it('renders a password input', () => {
+        expect(shallow(<FormComponent />).find('#password').length).toEqual(1)
+    })
+
+    it('renders the third input', () => {
+        expect(shallow(<FormComponent />).find('#another-input-field').length).toEqual(1)
+    })
+
+    it('renders the fourth input', () => {
+        expect(shallow(<FormComponent />).find('#last-input-field').length).toEqual(1)
+    })
+
 })
 
+describe('Email input', () => {
+  
+    it('should respond to change event and change the state of FormComponent', () => {
+     
+     const wrapper = shallow(<FormComponent />);
+     wrapper.find('#email').simulate('change', {target: {name: 'email', value: 'blah@gmail.com'}});
+     
+    expect(wrapper.state('email')).toEqual('blah@gmail.com');
+    })
+})
+   
+  describe('Password input', () => {
+    
+    it('should respond to change event and change the state of FormComponent', () => {
+     
+     const wrapper = shallow(<FormComponent />);
+     wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}});
+     
+     expect(wrapper.state('password')).toEqual('cats');
+    })
 
+    describe('anotherInputField input', () => {
+  
+        it('should respond to change event and change the state of FormComponent', () => {
+         
+         const wrapper = shallow(<FormComponent />);
+         wrapper.find('#another-input-field').simulate('change', {target: {name: 'anotherInputField', value: 'my third value'}});
+         
+        expect(wrapper.state('anotherInputField')).toEqual('my third value');
+        })
+    })
+
+    describe('lastInputField input', () => {
+  
+        it('should respond to change event and change the state of FormComponent', () => {
+         
+         const wrapper = shallow(<FormComponent />);
+         wrapper.find('#last-input-field').simulate('change', {target: {name: 'lastInputField', value: 'my fourth value'}});
+         
+        expect(wrapper.state('lastInputField')).toEqual('my fourth value');
+        })
+    })
+})
